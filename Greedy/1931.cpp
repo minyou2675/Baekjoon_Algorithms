a2b60d4145cdf42bfa9391fa92a0621b7547a830
[minyou2675] <[kymyy2675@gmail.com]>
2022-11-20 20:12:14 +0900

    update

---
+++ Greedy/1931.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void){
+        int N;
+        int before,next;
+        vector<pair<int,int>> list;
+        int min = 0;
+        int cnt = 0;
+        int i,j;
+        cin >> N;
+
+        for(i=0; i < N; i++){
+        int n,m;
+	scanf("%d %d",&n,&m);
+	list.push_back(make_pair(m,n));
+
+        }
+        sort(list.begin(),list.end());
+
+        for(i = 0; i < N; i++){
+        if(list[i].second >= before){
+                cnt++;
+                before = list[i].first;
+        }
+
+        }
+
+        cout << cnt;
+
+        return 0;
+}
+                                 

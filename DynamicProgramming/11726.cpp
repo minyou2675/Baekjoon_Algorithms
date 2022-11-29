3c7f4ba12c8672894c78e48e6948fe1aaa7371aa
=minyou2675 <kymyy2675@gmail.com>
2022-11-29 20:44:37 +0900

    update

--- DynamicProgramming/11726.cpp
+++ DynamicProgramming/11726.cpp
@@ -1,25 +1,34 @@
-#include <iostream>
 #include <cstdio>
-#include <algorithm>
+#include <vector>
+#include <queue>
 
 using namespace std;
+queue <int> q;
 
 int block(int n){
 	if(n == 1)
 		return 1;
 	else if(n == 2)
 		return 2;
-	else
-		return block(n-1)+block(n-2);
+	else{
+		int result =  block(n-1)+block(n-2);
+		q.push(result);
+		return 0;
+	}
 
 }
 
 
 int main(void){
-
+	int sum = 0;
 	int N;
-	cin >> N; 
-	printf("%d",block(N)%10007);
+	scanf("%d",&N); 
+	block(N);
+	while(!q.empty()){
+		sum += q.front();
+		q.pop();
+	}
+	printf("%d",sum%10007);
 
 
 

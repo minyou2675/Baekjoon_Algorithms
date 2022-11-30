0cca02bb927cb7b68d4aece18f0c2ff415cfd551
=minyou2675 <kymyy2675@gmail.com>
2022-11-30 21:52:36 +0900

    ffffftry

--- DynamicProgramming/1463.cpp
+++ DynamicProgramming/1463.cpp
@@ -10,22 +10,27 @@ int lst[1000001];
 int min(int n){
 	if(lst[n])
 		return lst[n];
-	 if(n==1)
+	if(n==1)
 		return 0;
-	 if(n%3 == 0 && n&2 == 0){
+	else  if(n%3 == 0 && n&2 == 0){
 		int minimum;
-		
 		minimum =  min(n/3) < min(n/2) ? min(n/3)+1 : min(n/2)+1;
 		return lst[n] = minimum < min(n-1)+1 ? minimum : min(n-1)+1; 
 	}
-	 if(n%3 == 0)
-		return lst[n] = min(n/3) < min(n-1) ? min(n/3)+1 : min(n-1)+1;
-	 if(n%2 == 0)
-		return lst[n] = min(n/2) < min(n-1) ? min(n/2)+1 : min(n-1)+1;
-	else
-		return lst[n] = min(n-1)+1;
+	 else  if(n%3 == 0){
+		 lst[n] = min(n/3) < min(n-1) ? min(n/3)+1 : min(n-1)+1;
+	 }
+	 else if(n%2 == 0){
+		 lst[n] = min(n/2) < min(n-1) ? min(n/2)+1 : min(n-1)+1;
+	 }
+	 
+	 else
+		 lst[n] = min(n-1)+1;
+
+	return lst[n]; 
+	 
 	
-	return lst[n];
+	
 }
 
 

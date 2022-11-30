133fc9c8bfc23d8adb11a1cbadd10665655df661
=minyou2675 <kymyy2675@gmail.com>
2022-11-30 21:07:16 +0900

    new create

---
+++ DynamicProgramming/1463.cpp
@@ -0,0 +1,44 @@
+#include <cstdio>
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int lst[1000000];
+
+
+int min(int n){
+	if(lst[n])
+		return lst[n];
+	else if(n==1)
+		return 0;
+	if(n%3 == 0 && n&2 == 0){
+		int minimum;
+		cout << n;
+		minimum =  min(n/3) < min(n/2) ? min(n/3)+1 : min(n/2)+1;
+		return lst[n] = minimum < min(n-1)+1 ? minimum : min(n-1)+1; 
+	}
+	else if(n%3 == 0)
+		return lst[n] = min(n/3) < min(n-1) ? min(n/3)+1 : min(n-1)+1;
+	else if(n%2 == 0)
+		return lst[n] = min(n/2) < min(n-1) ? min(n/2)+1 : min(n-1)+1;
+	else
+		return lst[n] = min(n-1)+1;
+	
+	return lst[n];
+}
+
+
+
+
+int main(void){
+
+	int N;
+	cin >> N; 
+	
+	int result = min(N);
+	printf("%d",result);
+
+
+	return 0;
+}

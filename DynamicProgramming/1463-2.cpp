067416e1cbf23a93c0ea323bab339b4a0086e1bd
=minyou2675 <kymyy2675@gmail.com>
2022-11-30 23:37:35 +0900

    pleasegit add .!

--- DynamicProgramming/1463-2.cpp
+++ DynamicProgramming/1463-2.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
+#define INF 10000000
 
 using namespace std;
 
 int lst[1000001];
-
-int opt(int n){
-	if(lst[n])
-		return lst[n];
-	if(n == 1)
-		return 1;
-	 if(n % 3 == 0)
-		return lst[n] = opt(n/3)+1;
-	 if(n % 2 == 0){
-		return lst[n] = opt(n/2)+1 < opt(n/3)+1 ? opt(n/2)+1:opt(n/3)+1;
-	}
-	 else
-		return lst[n] = opt(n-1)+1;
-	return lst[n];
 	
 
-
-}
-
-
+int opt(int n){
+	
+	if(lst[n])
+		return lst[n];
+	else if(n == 1)
+		return 0;
+	else{
+	int a = INF; int b = INF; int c = INF;
+	if(n % 3 == 0)  a = opt(n/3)+1;
+	if(n % 2 == 0)  b = opt(n/2)+1;
+	c = opt(n-1)+1;
+	lst[n] = min({a,b,c});
+	return lst[n];
+	}
+	
+	
+	}
 
 int main(void){
 
 	int N;
 	cin >> N;
-	lst[0] = 1;
 	int result = opt(N);
-	printf("%d",result);
+	cout << result; 
 
 
 	return 0;

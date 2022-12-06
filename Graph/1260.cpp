125d8da66dd485d3c53158e3be33e0db94afe9e2
minyou2675 <kymyy2675@gmail.com>
2022-12-06 12:04:37 +0900

    update

--- Graph/1260.cpp
+++ Graph/1260.cpp
@@ -2,50 +2,58 @@
 #include <vector>
 #include <queue>
 #include <stack>
+#include <cstring>
 #include <algorithm>
 #include <iostream>
 
-vector <int> vec;
+using namespace std;
+vector <int> vec[10001];
 queue <int> q; 
 queue <int> lst;
-stack <int> stk;
+bool check[1001];
 int N,M,V;
-bool visit[1000]; 
+int cnt = 0;
+bool visit[1001]; 
 
 void dfs(int n){
     if(!visit[n]){
     q.push(n);
+    cnt += 1;
     visit[n] = true;
+    sort(vec[n].begin(), vec[n].end());
     if(vec[n].size() != 0){
     for(int i = 0; i < vec[n].size(); i++){
-        dfs(vec[n][i]);
+	if(!visit[vec[n][i]])
+       	   dfs(vec[n][i]);
+    }	
     }
+   
     }
-    }
-    if(vec[n].size == 0){
-        while(!q.empty()){
-            cout << q.front();
-            q.pop();
-        }
-    }
-    
-    
+            
 
 }
 
 void bfs(int n){
+    if(n == V)
+	    cout << V << " ";
     if(!visit[n]){
+    
     visit[n] = true;
     sort(vec[n].begin(),vec[n].end());
     for(int i = 0; i < vec[n].size(); i++){
-        if(!visit[vec[n][i]])
-            lst.push(vec[n][i]);
+	   
+        if(!visit[vec[n][i]] && !check[vec[n][i]])
+           { lst.push(vec[n][i]);
+	     check[vec[n][i]] = true;
+		}
 
     }
-    cout << lst.front();
+   if(!lst.empty()){ 
+    printf("%d ", lst.front());
     int front = lst.front();
     lst.pop();
     bfs(front);
+   }
     
     
     }
@@ -55,19 +63,22 @@ void bfs(int n){
 }
 int main(void){
     cin >> N >> M >> V;
-    for(int i = 0; i < N; i++)  {
+    for(int i = 0; i < M; i++)  {
         int x,y;
-        cin >> x >> y
+        cin >> x >> y;
         vec[x].push_back(y);
+	vec[y].push_back(x);
 
     }
-    sort(vec.begin(),vec.end());
+    
     dfs(V);
+    while(!q.empty()){
+    cout << q.front() << " "; 
+    
+    q.pop();
+    }
     memset(visit,false,sizeof(visit));
     cout << "\n";
     bfs(V);
-
-
-
 
 }

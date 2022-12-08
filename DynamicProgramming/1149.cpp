cca567f2cae8edff3c45d7233c9e536f4a93fb44
minyou2675 <kymyy2675@gmail.com>
2022-12-08 20:27:11 +0900

    last

--- DynamicProgramming/1149.cpp
+++ DynamicProgramming/1149.cpp
@@ -6,62 +6,26 @@ using namespace std;
 #define INF 10000
 
 int rgb[1001][3];
-int color[1001];
-int N;
-int result[3];
-
-int min_cost(int n){
-    int minVal = INF;
-
-    if(n == N-1){
-	    if(color[n-1] == 0)
-		    minVal = rgb[n][1] > rgb[n][2] ? rgb[n][2] : rgb[n][1];
-	    else  if(color[n-1] == 1)
-		    minVal = rgb[n][0] < rgb[n][2] ? rgb[n][0] : rgb[n][2];
-	    else
-		    minVal = rgb[n][0] < rgb[n][1] ? rgb[n][0] : rgb[n][1];
-    	return minVal;
-    } 
-        
-   else  if(0 < n && n < N-1){
-       for(int i = 0; i < 3; i++){
-	    int temp;
-	    int cost;
-            color[n] = i;
-         
-        if(color[n-1] != color[n]){
-
-        	    temp = rgb[n][color[n]]+min_cost(n+1);
-           	    minVal = temp < minVal ? temp : minVal;
-	
-       }
-       }
-         
-        
-}
-
-
-return minVal;
-} 
-
+int cost[3];
 
 int main(void)
 {
+	int N;
+	cin >> N;
     
-    scanf("%d",&N);
-    for(int i =0; i < N; i++){
-        scanf("%d %d %d",&rgb[i][0],&rgb[i][1],&rgb[i][2]);
-        
-        
+    rgb[0][0] = 0;
+    rgb[0][1] = 0;
+    rgb[0][2] = 0;
+    for(int i = 1; i <= N; i++){
+	    cin >> cost[0] >> cost[1] >> cost[2];
+	    rgb[i][0] = min(rgb[i-1][1],rgb[i-1][2])+cost[0];
+	    rgb[i][1] = min(rgb[i-1][2],rgb[i-1][0])+cost[1];
+	    rgb[i][2] = min(rgb[i-1][0], rgb[i-1][1])+cost[2];
+
     }
-    int mini = INF;
-    for(int i = 0; i < 3; i++){
-        color[0] = i;
-	int temp; 
-        temp = rgb[0][color[0]]+min_cost(1);
-      	mini = mini < temp ? mini : temp;
-   }
-    printf("%d\n",mini);
+    sort(rgb[N],rgb[N]+3);
+   
+   cout << rgb[N][0];
 
 
 return 0;

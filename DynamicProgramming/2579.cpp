1900a8f394b541d9a716476fc75a84058ef13c0b
201711008 <kymyy2675@gmail.com>
2022-12-08 22:51:03 +0900

    2579

--- DynamicProgramming/2579.cpp
+++ DynamicProgramming/2579.cpp
@@ -10,16 +10,30 @@ int main(void){
     int N;
     cin >> N;
 
-    for(int i = 1; i < N; i++){
+    for(int i = 1; i <= N; i++){
         cin >> score[i];
 
     }
-    score[0] = 0;
-    for(int i = N; i > 2; i--){
-        score[i] = score[i] + min(score[i-2],score[i-1]);
+    int prev = 0;
+    for(int i = N-2; i >= 0; i--){
+        if(prev - i == 1)
+            return score[i];
+        
+        int num1 = score[i] + score[i+1];
 
+        int num2 = score[i] + score[i+2];
+        
+        if(max(num1,num2) == num1){
+            prev = i+1;
+        }
+        else
+            prev = i+2;
+        
     }
-    cout << score[N];
+    cout << score[0];
+    
+
+ 
 
 
 

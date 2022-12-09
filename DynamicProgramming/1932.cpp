4e904d12d3e4584e9d022ca7903b5d472b74a15c
minyou2675 <kymyy2675@gmail.com>
2022-12-10 02:15:30 +0900

    updt

--- DynamicProgramming/1932.cpp
+++ DynamicProgramming/1932.cpp
@@ -9,23 +9,25 @@ vector<int> dp[5000];
 
 int main(void){
     cin >> N;
+    
     for (int i = 0; i < N; i++){
-        for(int j=0; j<=j+i; j++){
+        for(int j=0; j<=i; j++){
             int n;
             cin >> n;
             tri[i].push_back(n);
         }
     }
-    for(int i = 0; i < N; i++)
-        dp[N-1][i] = tri[N-1][i];
+    
 
-    for(int i =N-2; i <= 0 ; i--){
-        for(int j=0; j+i; j++){
-            dp[i][j] = dp[i][j] + max(tri[i-1][j],tri[i-1][j+1]);
+    for(int i =N-2; i >= 0 ; i--){
+        for(int j=0; j<=i; j++){
+            tri[i][j] = tri[i][j] + max(tri[i+1][j],tri[i+1][j+1]);
         }
+	
     }
+    cout << tri[0][0] << endl;
 
-    cout << dp[0][0];
+    
 
 
     return 0;

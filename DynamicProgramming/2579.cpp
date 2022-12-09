7760bfb716f0e108e7dcfc24ef3cee6b0fa71655
minyou2675 <kymyy2675@gmail.com>
2022-12-09 23:04:04 +0900

    updt

--- DynamicProgramming/2579.cpp
+++ DynamicProgramming/2579.cpp
@@ -1,40 +1,46 @@
 #include <cstdio>
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
+#define INF 10000
 using namespace std; 
 
-int score[301];
+//vector <int> score;
+int score[10000][3]
 
 int main(void){
     int N;
     cin >> N;
-
+    score[0] = 0;
     for(int i = 1; i <= N; i++){
-        cin >> score[i];
+	    int n; 
+        cin >> n;
+	score.push_back(n);
 
     }
-    int prev = 0;
-    for(int i = N-2; i >= 0; i--){
-        if(prev - i == 1)
-            return score[i];
-        
-        int num1 = score[i] + score[i+1];
+    int r = N-1;
+    int l = N-2;
+    int idx = 0;
+    for(int i = r; i >= 0; i++){
+	    idx = 1; 
+	    if(r%2=0){
+	  
+  	  	r = r+max(score[i+1],score[i+2]);
+	    }
+	    else
+		    r = r+score[i+1]; 
 
-        int num2 = score[i] + score[i+2];
-        
-        if(max(num1,num2) == num1){
-            prev = i+1;
-        }
-        else
-            prev = i+2;
-        
-    }
-    cout << score[0];
+	    }
     
+    for(int i = l; l >= 0; l++){
+	    if(l%2=0){
+	    	l = l+max(score[i+1],score[i+2]);
+	    }
+	    else
+		    l = l+score[i+1];
+    }
 
- 
-
-
+	return 0;
 
 }

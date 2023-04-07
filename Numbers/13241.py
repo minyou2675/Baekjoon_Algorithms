6d2a082f8500eefece37b7c53882c99d2ffd334d
minyou2675 <kymyy2675@gmail.com>
2023-04-07 15:32:13 +0900

    add 13241.py

---
+++ Numbers/13241.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+def GCD(x,y):
+    while y:
+        x , y = y, x%y
+    return x
+
+def LCM(x,y):
+    return (x*y) // GCD(x,y)
+
+a , b = map(int, input().split())
+
+print(LCM(a,b))
--- Numbers/1934.py
+++ Numbers/1934.py
@@ -1,24 +1,24 @@
-    import sys
+import sys
 
-    input = sys.stdin.readline
+input = sys.stdin.readline
 
-    n = int(input())
+n = int(input())
 
-    lst = []
+lst = []
 
 
-    def GCD(x,y):
-        while(y):
-            x, y = y, x%y
-        return x
-    def LCM(x,y):
-        return (x*y) // GCD(x,y)
+def GCD(x,y):
+    while(y):
+        x, y = y, x%y
+    return x
+def LCM(x,y):
+    return (x*y) // GCD(x,y)
 
-    for i in range(n):
-        a,b = map(int,input().split())
-        lst.append(LCM(a,b))
-        
-    for i in lst:
-        print(i)
+for i in range(n):
+    a,b = map(int,input().split())
+    lst.append(LCM(a,b))
+    
+for i in lst:
+    print(i)
         
         
